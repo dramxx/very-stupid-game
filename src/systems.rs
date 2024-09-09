c7a8c40a02548408acfe7296c46dc4011af7255e
Drahoslav Madar <drahoslav.madar@gmail.com>
2024-09-09 16:50:46 +0200

    warning fixes

--- src/systems.rs
+++ src/systems.rs
@@ -68,7 +68,7 @@ pub fn handle_game_over(
     mut game_over_event_reader: EventReader<GameOver>,
     mut next_app_state: ResMut<NextState<AppState>>,
 ) {
-    for event in game_over_event_reader.iter() {
+    for _event in game_over_event_reader.iter() {
         next_app_state.set(AppState::GameOver);
     }
 }
